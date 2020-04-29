39d44c88c762cfe6fa230febdc9dda37f7068fe3
Serhiy Bardysh <sbardysh@tutanota.com>
2020-04-29 02:28:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ end
 def valid_move?(board, index)
     if board[index] == false && between?[0,8]
       
-    #else #board[index] == true && between?[0,8]
-      #false
+    else board[index] == true && between?[0,8]
+    
     end
 end
