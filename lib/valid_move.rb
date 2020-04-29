008af48fa19970057694faf0ad1487a1b08d461a
Serhiy Bardysh <sbardysh@tutanota.com>
2020-04-29 01:52:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,8 +6,9 @@ def position_taken?(board, index)
   end
 end
 
-def valid_move?(board,index)
-    if board[index].between?[0,8]
-      true
-    end
-end
+#def valid_move?(board,index)
+#    if board[index] == 
+#
+#      true
+#    end
+#end
